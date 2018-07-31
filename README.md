e77d17799986b07424322dd794de84e97df6e758
Trevor Umeda <tumeda@riotgames.com>
2018-07-30 20:16:46 -0700

    Some basic twitch stream stuff

---
+++ README.md
@@ -0,0 +1,13 @@
+# Hayatebot
+
+A [discord.js](https://discord.js.org/) bot that does goofy things.
+
+### Reactions
+
+Will post user entered reaction images. Due to the magic of discord hayate can just post
+links and discord will handle the rendering of them. Thus we don't have to worry about uploading
+them anywhere.
+
+Hayate will pull from a grails service hosted on heroku to get a random image that fits a "tag"
+
+### Twitch?
--- hayate.js
+++ hayate.js
@@ -1,18 +1,183 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const twitchConfig = __dirname + "/twitchConfig.json"
 const fs = require("fs");
 const http = require("http");
+const https = require("https");
 const _ = require('underscore');
-client.login(config.token);
+const timeout = 2 * 60 * 100;
+const interval = 30 * 1000; // The interval between polling twitch
+
+var servers = [];
+
+client.login(config.token).then((token)=>{
+    if(token){
+        printLog("Logged in with token " + token);
+        printLog("Reading Twitch config file file " + twitchConfig);
+        // var file = fs.readFileSync(channelPath, {encoding:"utf-8"});
+        // servers = JSON.parse(file);
+        var twitchConfigFile = fs.readFileSync(twitchConfig, {encoding:"utf-8"});
+        console.log(twitchConfigFile)
+        servers = JSON.parse(twitchConfigFile);
+        console.log(servers.twitchChannels)
+        printLog(servers)
+
+        // tick once on startup
+        checkTwitchChannelsTick();
+        setInterval(checkTwitchChannelsTick, interval);
+    }else{
+        printLog("An error occured while loging in:", err);
+        process.exit(1);
+    }
+});;
+
+/*
+  Twitch
+*/
+function callApi(server, twitchChannel, callback, getStreamInfo){
+    var opt;
+    try {
+        var apiPath;
+        // if(getStreamInfo){
+        //   //api.twitch.tv/helix/streams?user_login=pandaxgaming
+        //     apiPath = "/helix/streams?user_login=" + twitchChannel.name.trim();
+        // }else{
+        //     apiPath = "/helix/users?login=" + twitchChannel.name.trim();
+        // }
+        if(getStreamInfo){
+          printLog(twitchChannel)
+            apiPath = "/kraken/streams/" + twitchChannel.id;
+        }else{
+            apiPath = "/kraken/channels/" + twitchChannel.id;
+        }
+        opt = {
+            host: "api.twitch.tv",
+            path: apiPath,
+            headers: {
+                "Client-ID": config.twitchClientID,
+                Accept: "application/vnd.twitchtv.v5+json"
+
+            }
+        };
+    }
+    catch(err){
+        printLog(err);
+        return;
+    }
+    console.log(opt)
+
+    https.get(opt, (res)=>{
+        var body = "";
+
+        res.on("data", (chunk)=>{
+            body += chunk;
+        });
+
+        res.on("end", ()=>{
+            var json;
+            try {
+                json = JSON.parse(body);
+            }
+            catch(err){
+                printLog(err);
+                return;
+            }
+            if(json.status == 404){
+                callback(server, undefined, undefined);
+            }else{
+                callback(server, twitchChannel, json);
+            }
+        });
+
+    }).on("error", (err)=>{
+        printLog(err);
+    });
+}
+
+
+function apiCallback(server, twitchChannel, res){
+    // if(res && !twitchChannel.online && res.data.length > 0 &&
+    if(res && !twitchChannel.online && res.stream &&
+       twitchChannel.timestamp + timeout <= Date.now()){
+        try {
+            var channels = [], defaultChannel;
+            var guild = client.guilds.find("name", server.name);
+
+            if(server.discordChannels.length === 0){
+                defaultChannel = guild.channels.find("type", "text");
+            }else{
+                for(let i = 0; i < server.discordChannels.length; i++){
+                    channels.push(guild.channels.find("name", server.discordChannels[i]));
+                }
+            }
+            // var stream = res.data[0]
+            // var embed = new Discord.RichEmbed()
+            //             .setColor("#9689b9")
+            //             .setTitle(stream.title.replace(/_/g, "\\_"))
+            //             .setURL(res.stream.channel.url)
+            //             .setDescription("**" + res.stream.channel.status +
+            //                             "**\n" + res.stream.game)
+            //             .setImage(res.stream.preview.large)
+            //             .setThumbnail(res.stream.channel.logo)
+            //             .addField("Viewers", stream.viewer_count, true)
+
+            var embed = new Discord.RichEmbed()
+                          .setColor("#9689b9")
+                          .setTitle(res.stream.channel.display_name.replace(/_/g, "\\_"))
+                          .setURL(res.stream.channel.url)
+                          .setDescription("**" + res.stream.channel.status +
+                                          "**\n" + res.stream.game)
+                          .setImage(res.stream.preview.large)
+                          .setThumbnail(res.stream.channel.logo)
+                          .addField("Viewers", res.stream.viewers, true)
+                          .addField("Followers", res.stream.channel.followers, true);
+            if(channels.length !== 0){
+                for(let i = 0; i < channels.length; i++){
+                    channels[i].send(embed).then(
+                        printLog("Sent embed to channel '" + channels[i].name +
+                              "'."));
+                }
+                twitchChannel.online = true;
+                twitchChannel.timestamp = Date.now();
+            }else if(defaultChannel){
+                defaultChannel.send(embed).then(
+                    printLog("Sent embed to channel '" + defaultChannel.name +
+                          "'.")
+                );
+                twitchChannel.online = true;
+                twitchChannel.timestamp = Date.now();
+            }
+        }
+        catch(err){
+            printLog(err);
+        }
+    }else if(res.stream === null){
+        twitchChannel.online = false;
+    }
+}
+function checkTwitchChannelsTick(){
+    for(let i = 0; i < servers.length; i++){
+        for(let j = 0; j < servers[i].twitchChannels.length; j++){
+            if(servers[i].twitchChannels[j]){
+                callApi(servers[i], servers[i].twitchChannels[j], apiCallback, true);
+            }
+        }
+    }
+}
 
 client.on("ready", () => {
   console.log("I am ready!");
   reloadTags();
 });
 
+
 // Global state.
 let tags = null
+
+/*
+  Reaction images
+*/
 
 // Read configuration from the environment.
 const tagUrl = "http://vast-castle-1062.herokuapp.com/tags"
@@ -35,6 +200,7 @@ var reloadTags = function() {
 
 client.on("message", (message) => {
 
+
   if (message.author.bot) return;
 
   if (message.content.startsWith("ping")) {
@@ -48,7 +214,6 @@ client.on("message", (message) => {
       reloadTags();
       message.channel.send("Tags reloaded!");
   }
-
 
   if (message.content.startsWith("!logInfo")) {
     console.log(tags)
@@ -70,10 +235,8 @@ client.on("message", (message) => {
               body += data;
             });
             res.on("end", () => {
-              if(!body.startsWith("<")) {
-                console.log(body)
-                message.channel.send(body)
-              }
+              console.log(body)
+              message.channel.send(body)
             });
           });
         }
@@ -94,28 +257,47 @@ client.on("message", (message) => {
                   body += data;
                 });
                 res.on("end", () => {
-                  if(!body.startsWith("<")) {
-                    console.log(body)
-                    message.channel.send(body)
-                  }              
+                  console.log(body)
+                  message.channel.send(body)
                 });
               });
               break;
-              //Get a random image from server and post it!
-              // robot.http(imgUrl + tag)
-              // .get() (err, res, body) ->
-              //   if res.statusCode is 200
-              //     console.log("#{tag} - #{body}")
-              //     msg.send "#{body}"
-              //   else
-              //     tags = []
-              // break
             }
-          }
-
           }
         }
       }
+    }
   }
-
 });
+
+/*
+  Utility
+*/
+function leadingZero(d){
+    if(d < 10){
+        return "0" + d;
+    }else{
+        return d;
+    }
+}
+// adds a timestamp before msg/err
+function printLog(msg, err){
+    var date = new Date();
+    var h = leadingZero(date.getHours());
+    var m = leadingZero(date.getMinutes());
+    var s = leadingZero(date.getSeconds());
+
+    console.log("[" + h + ":" + m + ":" + s + "]", msg);
+    if(err){
+        console.log(err);
+    }
+}
+
+function indexOfObjectByName(array, value){
+    for(let i = 0; i < array.length; i++){
+        if(array[i].name.toLowerCase().trim() === value.toLowerCase().trim()){
+            return i;
+        }
+    }
+    return -1;
+}
